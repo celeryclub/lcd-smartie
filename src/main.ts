6e943f4ac03549b8d54f231bd4c884fc1eaa1331
Steve Davis <steve@celery.club>
2023-04-02 19:13:14 +1000

    Convert project to TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
-const SerialPort = require("serialport");
+import { SerialPort } from "serialport";
 
 const DEFAULT_TTY_PATH = "/dev/ttyUSB0";
 const DEFAULT_WIDTH = 20;
 const DEFAULT_HEIGHT = 4;
 const COMMAND_DELAY = 40;
 
-class Smartie {
+export default class Smartie {
+  private lcd: SerialPort;
+  private width: number;
+  private height: number;
+
   constructor(ttyPath = DEFAULT_TTY_PATH, width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT) {
     this.lcd = new SerialPort({ path: ttyPath, baudRate: 9600 });
     this.width = width;
     this.height = height;
   }
 
-  async send(bytes) {
+  async send(bytes: number[]) {
     console.log("send", bytes);
     const buffer = Buffer.from([0xfe].concat(bytes));
     this.lcd.write(buffer);
@@ -27,32 +31,32 @@ class Smartie {
   }
 
   async backlightOff() {
-    await this.send(0x46);
+    await this.send([0x46]);
   }
 
-  async setBrightness(amount) {
+  async setBrightness(amount: number) {
     await this.send([0x98, amount]);
   }
 
-  async setContrast(amount) {
+  async setContrast(amount: number) {
     await this.send([0x50, amount]);
   }
 
-  async writeLine(data, line = 1) {
-    console.log("writeLine", data, line);
-    if (!line || line < 1 || line > this.height) {
-      line = 1;
+  async writeLine(message: string, lineNumber = 1) {
+    // console.log("writeLine", message, lineNumber);
+    if (!lineNumber || lineNumber < 1 || lineNumber > this.height) {
+      lineNumber = 1;
     }
 
-    data = data.padEnd(this.width).substring(0, this.width);
+    message = message.padEnd(this.width).substring(0, this.width);
 
     var arr = [];
-    for (var i = 0, l = data.length; i < l; i++) {
-      var ascii = data.charCodeAt(i);
+    for (var i = 0, l = message.length; i < l; i++) {
+      var ascii = message.charCodeAt(i);
       arr.push(ascii);
     }
 
-    await this.send([0x47, 0x01, line].concat(arr));
+    await this.send([0x47, 0x01, lineNumber].concat(arr));
   }
 
   async clearScreen() {
@@ -72,5 +76,3 @@ class Smartie {
   //   });
   // }
 }
-
-module.exports = Smartie;
