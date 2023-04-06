bb13609d49c41c0e8be9ebdc75f2894b66402466
Steve Davis <steve@celery.club>
2023-04-06 15:20:52 +1000

    Improve command line API

--- bin/smartie.js
+++ bin/smartie.js
@@ -4,52 +4,86 @@ import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import { Smartie } from "../dist/src/Smartie.js";
 
-const argv = yargs(hideBin(process.argv))
+yargs(hideBin(process.argv))
   .options({
     path: {
-      alias: "p",
       type: "string",
+      alias: "p",
       demandOption: true,
     },
-    backlight: {
-      type: "string",
-    },
-    brightness: {
-      alias: "b",
-      type: "number",
-    },
-    contrast: {
-      alias: "c",
-      type: "number",
-    },
-    line: {
-      alias: "l",
-      type: "number",
-    },
-    message: {
-      alias: "m",
-      type: "string",
-    },
-    clear: {
-      alias: "c",
+  })
+  .command({
+    command: "on",
+    desc: "Turn the backlight on",
+    handler: argv => {
+      const smartie = Smartie.fromPath(argv.path);
+      smartie.backlightOn();
     },
   })
+  .command({
+    command: "off",
+    desc: "Turn the backlight off",
+    handler: argv => {
+      const smartie = Smartie.fromPath(argv.path);
+      smartie.backlightOff();
+    },
+  })
+  .command({
+    command: "adjust",
+    desc: "Adjust brightness and contrast",
+    builder: yargs =>
+      yargs.options({
+        brightness: {
+          type: "number",
+          alias: "b",
+        },
+        contrast: {
+          type: "number",
+          alias: "c",
+        },
+      }),
+    handler: argv => {
+      if (!argv.brightness && !argv.contrast) {
+        throw new Error("Either brightness or contrast must be provided");
+      }
+
+      const smartie = Smartie.fromPath(argv.path);
+      if (argv.brightness) {
+        smartie.setBrightness(argv.brightness);
+      }
+      if (argv.contrast) {
+        smartie.setContrast(argv.contrast);
+      }
+    },
+  })
+  .command({
+    command: "write",
+    desc: "Write message to screen",
+    builder: yargs =>
+      yargs.options({
+        message: {
+          type: "string",
+          alias: "m",
+          demandOption: true,
+        },
+        line: {
+          type: "number",
+          alias: "l",
+          demandOption: true,
+        },
+      }),
+    handler: argv => {
+      const smartie = Smartie.fromPath(argv.path);
+      smartie.writeLine(argv.message, argv.line);
+    },
+  })
+  .command({
+    command: "clear",
+    desc: "Clear screen",
+    handler: argv => {
+      const smartie = Smartie.fromPath(argv.path);
+      smartie.clear();
+    },
+  })
+  .demandCommand()
   .help().argv;
-
-const smartie = Smartie.fromPath(argv.path);
-
-if (argv.backlight === "on") {
-  smartie.backlightOn();
-} else if (argv.backlight === "off") {
-  smartie.backlightOff();
-} else if (argv.brightness) {
-  // 0 to 255
-  smartie.setBrightness(argv.brightness);
-} else if (argv.contrast) {
-  // 0 to 255
-  smartie.setContrast(argv.contrast);
-} else if (argv.message) {
-  smartie.writeLine(argv.message, argv.line);
-} else if (argv.clear) {
-  smartie.clear();
-}
